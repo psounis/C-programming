ca32dc5b1f282c6477f1297e94c1a7fa8e445c57
psounis <psounis@gmail.com>
2019-07-21 18:16:33 +0300

    Create efarmogi1.c

---
+++ lesson14/efarmogi1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int fibonacci(int n);
+
+main()
+{
+	int n;
+	
+	printf("Dwse ton arithmo: ");
+	scanf("%d",&n);
+	
+	printf("O arithmos Fibonacci-%d einai %d", n, fibonacci(n));
+}
+
+int fibonacci(int n)
+{
+	static int count=0;
+	
+	count++;
+	printf("\n%d", count);
+	
+	if (n==1 || n==2)
+		return 1;
+	else 
+		return fibonacci(n-1)+fibonacci(n-2);
+		
+	
+}
