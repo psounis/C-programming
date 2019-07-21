783044ce1cc1a7fdeef904f15f745d4f69b3bf4b
psounis <psounis@gmail.com>
2019-07-21 18:09:29 +0300

    Create array.c

---
+++ lesson07/array.c
@@ -0,0 +1,15 @@
+/* array.c: Aplo programma me pinaka */
+
+#include <stdio.h>
+
+int main()
+{
+   int pin[3];
+   int sum;
+   
+   pin[0]=1; 
+   pin[1]=3;
+   pin[2]=4;
+   sum=pin[0]+pin[1]+pin[2];
+   printf("\n%d+%d+%d=%d",pin[0],pin[1],pin[2],sum); 
+}
