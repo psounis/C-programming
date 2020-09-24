079d7105ab18af48d1707004adde14d4f767fbff
psounis <psounis@gmail.com>
2020-09-24 20:25:06 +0300

    Update efarmogi3.c

--- lesson06/efarmogi3.c
+++ lesson06/efarmogi3.c
@@ -4,52 +4,42 @@ int isprime(int n);
 
 main()
 {
-	int i,start, finish;
+	int i, start, finish;
 	
-	/* Diavasma eisodou */
-	do{
+	do {
 		printf("Dwse tin arxi tou diastimatos: ");
-		scanf("%d",&start);
+		scanf("%d", &start);
 		
 		if (start<=0)
-			printf("Lathos Eisodos. Prepei na einai >0.");
-			
-	} while (start<=0);
-
-	do{
-		printf("Dwse to telos tou diastimatos: ");
-		scanf("%d",&finish);
-		
-		if (finish<=0)
-			printf("Lathos Eisodos. Prepei na einai >0.");
-			
-		if (finish<start)
-			printf("Lathos Eisodos. Prepei na einai >%d", start);
-			
-	} while (finish<=0 || finish<start);
+			printf("Lathos Eisodos. Prepei na einai >0");
+	} while(start<=0);
 	
-	/* Elegxos prwtwn arithmwn sto diastima */
+	
+	do {
+		printf("Dwse to peras tou diastimatos: ");
+		scanf("%d", &finish);
+		
+		if (!(start<=finish))
+			printf("Lathos Eisodos. Prepei na einai >%d(start)", start);
+	} while(!(start<=finish));
 	
 	for (i=start; i<=finish; i++)
-	{
 		if (isprime(i))
 			printf("\nTo %d einai prwtos", i);
-	}
-	
 }
 
 int isprime(int n)
 {
 	int i;
-	int check;
+	int check; 
 	
-	check=1; // o arithmos einai prwtos
+	if (n==0||n==1)
+		return 0;
+	
+	check=1;
 	for (i=2; i<=n/2; i++)
 		if (n%i==0)
-			check=0; //o arithmos den einai prwtos
-	
-	if (check)
-		return 1;
-	else
-		return 0;
+			check=0;
+
+	return check;
 }
